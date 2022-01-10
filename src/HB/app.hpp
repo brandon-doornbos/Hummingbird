96b3843c05975638765b05a541058e1ae85c037d
Brandon Doornbos <b.doornbos@protonmail.com>
2022-01-10 17:45:18 +0100

    Setup clang-format

--- src/HB/app.hpp
+++ src/HB/app.hpp
@@ -1,53 +1,52 @@
 #ifndef _HB_APP
 #define _HB_APP
 
-#include <cstdlib>
+#include <cstdint>
 #include <vector>
 
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-namespace HB
-{
-    struct AppInfo
-    {
-        uint32_t width;
-        uint32_t height;
-        char const *name;
+namespace HB {
+
+struct AppInfo {
+    uint32_t width;
+    uint32_t height;
+    char const* name;
+};
+
+class App {
+public:
+    void run();
+    App(AppInfo);
+    ~App();
+
+private:
+    std::vector<char const*> const m_validation_layers = {
+        "VK_LAYER_KHRONOS_validation"
     };
-
-    class App
-    {
-    public:
-        void run();
-        App(AppInfo);
-        ~App();
-
-    private:
-        std::vector<char const *> const m_validation_layers = {
-            "VK_LAYER_KHRONOS_validation"};
 #ifdef NDEBUG
-        bool const m_enable_validation_layers = false;
+    bool const m_enable_validation_layers = false;
 #else
-        bool const m_enable_validation_layers = true;
+    bool const m_enable_validation_layers = true;
 #endif
-        AppInfo m_app_info;
-        GLFWwindow *m_window;
-        VkInstance m_instance;
-        VkPhysicalDevice m_physical_device = VK_NULL_HANDLE;
+    AppInfo m_app_info;
+    GLFWwindow* m_window;
+    VkInstance m_instance;
+    VkPhysicalDevice m_physical_device = VK_NULL_HANDLE;
 
-        struct QueueFamilyIndices;
+    struct QueueFamilyIndices;
 
-        bool checkValidationLayerSupport() const;
-        void initWindow();
-        void createInstance();
-        void initVulkan();
-        void pickPhysicalDevice();
-        QueueFamilyIndices findQueueFamilies(VkPhysicalDevice);
-        bool isDeviceSuitable(VkPhysicalDevice);
-        void loop();
-    };
+    bool checkValidationLayerSupport() const;
+    void initWindow();
+    void createInstance();
+    void initVulkan();
+    void pickPhysicalDevice();
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice);
+    bool isDeviceSuitable(VkPhysicalDevice);
+    void loop();
+};
+
 }
 
 #endif
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
-#include <cstdlib>
+#include <cstdint>
 #include <vector>
 
 #include "HB/app.hpp"
 
-constexpr char const *PROJECT_NAME = "hummingbird";
+constexpr char const* PROJECT_NAME = "hummingbird";
 constexpr uint32_t const WIDTH = 1280;
 constexpr uint32_t const HEIGHT = 720;
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     (void)argc;
     (void)argv;
 
-    HB::AppInfo app_info{};
+    HB::AppInfo app_info {};
     app_info.width = WIDTH;
     app_info.height = HEIGHT;
     app_info.name = PROJECT_NAME;
-    HB::App app{app_info};
+    HB::App app { app_info };
     app.run();
 
     return 0;
 }
-

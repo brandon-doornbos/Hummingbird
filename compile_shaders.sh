017ba2979a2a274e4050540d6fa28e925f9c315c
Brandon Doornbos <b.doornbos@protonmail.com>
2022-04-02 23:12:11 +0200

    Add shaders and meson run target for compiling them

---
+++ compile_shaders.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir "${MESON_BUILD_ROOT}/shaders"
+
+cd "${MESON_SOURCE_ROOT}/src/shaders/"
+
+glslc -fshader-stage=vert "vert.glsl" -o "${MESON_BUILD_ROOT}/shaders/vert.spv"
+glslc -fshader-stage=frag "frag.glsl" -o "${MESON_BUILD_ROOT}/shaders/frag.spv"
